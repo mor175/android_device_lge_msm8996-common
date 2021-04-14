7b9841837c76507e7eea93c764fc25846f1fc222
Chirayu Desai <chirayudesai1@gmail.com>
2021-04-15 02:09:34 +0530

    msm8996-common: rro_overlays: Move CarrierConfig to RRO
    Change-Id: Ia57acd244608848f70fdcc30d5694c919db60671

--- msm8996.mk
+++ msm8996.mk
@@ -341,6 +341,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    CarrierConfigOverlay \
     android.hardware.radio@1.4 \
     android.hardware.radio.config@1.0 \
     libprotobuf-cpp-full \
